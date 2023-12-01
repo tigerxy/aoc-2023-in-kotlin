5bde85703cb5745ebec281db64a378c44d203082
Roland Greim <github@rolandgreim.de>
2023-12-01 21:09:32 +0100

    :sparkles: solve day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,35 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>): Int =
+        input.sumOf { line ->
+            "${
+                line.first {
+                    it.isDigit()
+                }
+            }${
+                line.last {
+                    it.isDigit()
+                }
+            }".toInt()
+        }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val textNumbers = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+        val numbers =  (1..9).map { it.toString() } + textNumbers
+        return input
+            .sumOf { line ->
+                val first = line.findAnyOf(numbers)!!.second
+                val second = line.findLastAnyOf(numbers)!!.second
+
+                val convertToNum = fun(s: String): Int = s.toIntOrNull() ?: (textNumbers.indexOf(s) + 1)
+
+                convertToNum(first) * 10 + convertToNum(second)
+            }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    part2(testInput).println()
+    check(part2(testInput) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
